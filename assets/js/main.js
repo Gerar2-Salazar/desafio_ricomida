c17e8a4f4b62289d3b1b6db16b23318998af3600
Gerardo Salazar <157648035+Gerar2-Salazar@users.noreply.github.com>
2025-04-25 01:45:16 +0000

    ricomida

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,22 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$('#btn').click(function(){})
+    alert("boton clickeado!!!")
+
+$('.hover').hover(
+    function() {$(this).css('background-color','yellow')}
+    function() {$(this).css('background-color','')},
+)
+
+$('h2').dblclick(function(){
+    $(this).css('color','red')   
+}
+
+$('btntoggle').click(function(){
+    $('#contenido').toggle()
+})
